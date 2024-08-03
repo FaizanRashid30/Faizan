986924c4a31dad29a2679059880a2cde938b93cd
Faizan Rashid <109276572+FaizanRashid30@users.noreply.github.com>
2024-08-03 21:12:05 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('dp.png'))
@@ -15,5 +14,5 @@ st.info('Code animation create video into animation')
 
 icon_size = 20
 
-st_button('tiktok', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
-st_button('tiktok', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
+st_button('youtube', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
+st_button('youtube', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
