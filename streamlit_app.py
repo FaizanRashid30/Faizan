76cee510f4f6b09ba7315a67bd47adce675e5b91
Faizan Rashid <109276572+FaizanRashid30@users.noreply.github.com>
2024-08-03 21:15:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ st.info('Code animation create video into animation')
 
 icon_size = 20
 
-st_button('youtube', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
+st_button('turtle', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
 st_button('youtube', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
