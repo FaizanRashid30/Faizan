d13da809c5bb32cb91fd393145e6c80060f4b934
Faizan Rashid <109276572+FaizanRashid30@users.noreply.github.com>
2024-08-03 21:21:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from st_functions import st_button, load_css
-from st_social_media_links import SocialMediaIcons
 from PIL import Image
 
 load_css()
@@ -16,13 +15,4 @@ st.info('Code animation create video into animation')
 icon_size = 20
 
 st_button('youtube', 'https://www.tiktok.com/@code.animation?is_from_webapp=1&sender_device=pc', 'Tiktok', icon_size)
-
-social_media_links = [
-    "https://www.facebook.com/ThisIsAnExampleLink",
-    "https://www.youtube.com/ThisIsAnExampleLink",
-    "https://www.instagram.com/ThisIsAnExampleLink",
-    "https://www.github.com/jlnetosci/st-social-media-links",
-]
-
-social_media_icons = SocialMediaIcons(social_media_links)
 
